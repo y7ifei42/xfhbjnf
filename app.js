63fd03d0fae46debfee63eb80389d4042830a395
y7ifei42 <24hs103@s.bgu.ac.jp>
2024-12-09 14:22:12 +0900

    app.js

--- app.js
+++ app.js
@@ -3,17 +3,17 @@
             var season = parseInt(document.getElementById('season-input').value, 10);
             var result = "味噌ラーメン"; // デフォルトの値
 
-            if (colour === "曇り" && season === 春) {
+            if (weather === "曇り" && season === 春) {
                 result = "醤油ラーメン";
-            } else if (colour === "曇り" && season === 冬) {
+            } else if (weather === "曇り" && season === 冬) {
                 result = "味噌ラーメン";
-            } else if (colour === "晴れ" && season === 冬) {
+            } else if (weather === "晴れ" && season === 冬) {
                 result = "豚骨ラーメン";
-            } else if (colour === "雨" && season === 秋) {
+            } else if (weather === "雨" && season === 秋) {
                 result = "塩ラーメン";
-            } else if (colour === "快晴" && season === 夏) {
+            } else if (weather === "快晴" && season === 夏) {
                 result = "担々麺";
-            } else if (colour === "雨" && season === 夏) {
+            } else if (weather === "雨" && season === 夏) {
                 result = "つけ麺";
             } 
 
